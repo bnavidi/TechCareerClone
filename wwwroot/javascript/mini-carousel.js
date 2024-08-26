54264900364808dbe074cecebd27136c5baa2946
Benan Navidi <benannavidi@gmail.com>
2024-08-26 18:29:25 +0300

    etkinlik kartları eklendi.

---
+++ wwwroot/javascript/mini-carousel.js
@@ -0,0 +1,28 @@
+const slider = document.querySelector(".mini-carousel");
+const slides = document.querySelectorAll(".mini-carousel img");
+const prevBtn = document.querySelector(".carousel-control-prev");
+const nextBtn = document.querySelector(".carousel-control-next");
+
+let currentIndex = 0;
+
+function showSlide(index){
+    if(index >= slides.lenght){
+        currentIndex = 0;
+    }else if(index < 0){
+        currentIndex = slides.length - 1;
+    }else{
+        currentIndex = index;
+    }
+
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+nextBtn.addEventListener("click", ()=>{
+    showSlide(currentIndex + 1);
+});
+prevBtn.addEventListener("click", ()=>{
+    showSlide(currentIndex - 1);
+});
+setInterval(() => {
+    showSlide(currentIndex + 1);
+}, 3000);
